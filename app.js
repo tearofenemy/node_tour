2b3cc16dcd2abc19ca365f95d8b20c36395baf5a
tearofenemy <calm.snow98@gmail.com>
2020-11-04 17:12:36 +0300

    implements post request to add new tour

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-import fs from 'fs';
-import express from 'express';
-
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser').json();
 const {log} = console;
 const port = process.env.port || 8000;
 const app = express();
 
+app.use(express.json());
+
 
 const tours = JSON.parse(
-    fs.readFileSync('./dev-data/data/tours-simple.json')
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
 
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json(
@@ -16,11 +19,27 @@ app.get('/api/v1/tours', (req, res) => {
             status: 'success', 
             results: tours.length,
             data: {
-                tours: tours
+                tours
             }
-        })
+        });
 });
 
+app.post('/api/v1/tours', bodyParser, (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = {id: newId, ...req.body};
+
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        if(err) throw err;
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    });
+});
 
 
 app.listen(port, () => {
