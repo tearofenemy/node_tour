02b5724ec5e25ddb5da98c48f555fa4a8303f89b
tearofenemy <calm.snow98@gmail.com>
2020-11-06 16:22:08 +0300

    implement update & delete methods

--- app.js
+++ app.js
@@ -22,8 +22,25 @@ app.get('/api/v1/tours', (req, res) => {
         });
 });
 
-app.get('/api/v1/tours/:id', (req, res) => {
 
+app.post('/api/v1/tours', bodyParser, (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = {id: newId, ...req.body};
+
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        if(err) throw err;
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
     const id = req.params.id * 1;
     const tour = tours.find(el => el.id === id);
 
@@ -45,23 +62,38 @@ app.get('/api/v1/tours/:id', (req, res) => {
         });
 });
 
-app.post('/api/v1/tours', bodyParser, (req, res) => {
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = {id: newId, ...req.body};
-
-    tours.push(newTour);
-
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        if(err) throw err;
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
+app.patch('/api/v1/tours/:id', bodyParser, (req, res) => {
+    if(req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'failed',
+            message: 'Invalid ID'
         });
+
+    }  
+   
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: "<Updated tour/>"
+        }
     });
 });
 
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+    if(req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'failed',
+            message: 'Invalid ID'
+        });
+
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    });
+});
 
 app.listen(port, () => {
     log(`App starting on localhost:${port}`);
