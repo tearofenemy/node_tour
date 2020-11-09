ceb8b397a5b825d8ab39e6c17c152b13b033da70
tearofenemy <calm.snow98@gmail.com>
2020-11-09 22:49:55 +0300

    implements select some fields to display from query

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -22,6 +22,14 @@ exports.getTours = async (req, res) => {
             query = query.sort('-createdAt');
         }
 
+        //SELECT FIELDS
+        if(req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields);
+        } else {
+            query = query.select('-__v');
+        }
+        
         const tours = await query;
 
         res.status(200).json({
