2b2db23fbb026f1f5de380bd476991f945960f53
tearofenemy <calm.snow98@gmail.com>
2020-11-09 23:44:39 +0300

    implements paginate and limit tours per page

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -30,6 +30,18 @@ exports.getTours = async (req, res) => {
             query = query.select('-__v');
         }
         
+        //LIMITING & PAGINATE
+        const page = req.query.page * 1 || 1;
+        const limit = req.query.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+
+        query.skip(skip).limit(limit);
+
+        if(req.query.page) {
+            const numTours = await Tour.countDocuments();
+            if(skip >= numTours) throw new Error("Thi page does not exist");
+        }
+
         const tours = await query;
 
         res.status(200).json({
